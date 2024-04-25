7085c8e385ab94f316bd9bc16969b824e8e05dfe
sanchayan-das_psl <sanchayan_das@persistent.com>
2024-04-25 16:45:17 +0530

    code update for docker file

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -10,7 +10,7 @@ module.exports = {
   // password: "postgres",
   // database: "nest_crud",
     type: 'postgres',
-    host: 'db',
+    host: '10.0.0.5',
     port: 5432,
     username: 'postgres',
     password: 'postgres',
