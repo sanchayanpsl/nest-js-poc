5dce6de1be3f855f2c5b748500aea7dfbc6e44fd
sanchayan-das_psl <sanchayan_das@persistent.com>
2024-04-26 16:22:14 +0530

    code update for pg

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -10,7 +10,7 @@ module.exports = {
   // password: "postgres",
   // database: "nest_crud",
   type: "postgres",
-  host: "10.0.0.5",
+  host: "172.18.0.2",
   port: 5432,
   username: "postgres",
   password: "postgres",
